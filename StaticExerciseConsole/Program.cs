a3a3912aad2d22fc21789e587094855ee92eab35
Supersagee <williamdsage@gmail.com>
2022-02-07 19:25:38 -0600

    first commit

---
+++ StaticExerciseConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(84));
+            
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(33));
+           
+        }
+    }
+}
---
+++ StaticExerciseConsole/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExerciseConsole
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double userTemp)
+        {
+            return (userTemp - 32) * .5556;
+        }
+
+        public static double CelsiusToFahrenheit(double userTemp)
+        {
+            return (userTemp * 1.8) + 32; 
+        }
+
+    }
+}
